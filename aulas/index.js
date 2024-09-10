72570a7f4cd8a07022d1977b9b08e52ccbe8716d
Leandro Carneiro <ldiascarneiro@yahoo.com.br>
2024-09-10 11:31:30 -0300

    update

--- aulas/index.js
+++ aulas/index.js
@@ -1,4 +1,4 @@
-console.log('teste')
-console.log('teste2')
-console.log('teste3')
-console.log('teste4')
+console.log('teste');
+console.log('teste2');
+console.log('teste3');
+console.log('teste4');
